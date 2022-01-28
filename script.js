d236b7c6fa62c8d70ede35c91828bb3a5fa70d0e
DiogoBatista1 <diogobatista330@gmail.com>
2022-01-28 06:46:22 -0300

    Botoes codificador, decodificador e copiar configurados.

--- script.js
+++ script.js
@@ -1,39 +1,50 @@
-
-/* Regras Codificador: 
-"e" é convertido para "enter" 
-"i" é convertido para "imes"
-"a" é convertido para "ai"
-"o" é convertido para "ober"
-"u" é convertido para "ufat"
-Apenas letras minúsculas
-Não permite acentuação   
-*/
-
-/* Regras Decodificador: 
-"enter" é convertido para "e" 
-"imes" é convertido para "i"
-"ai" é convertido para "a"
-"ober" é convertido para "o"
-"ufat" é convertido para "u"
-Apenas letras minúsculas
-Não permite acentuação     
-*/
-var items = [
-    ["a", "ai"],
+var chaves = [
     ["e", "enter"],
     ["i", "imes"],
+    ["a", "ai"],
     ["o", "ober"],
-    ["u", "ufat"]
-];
+    ["u", "ufat"],
+  ];
 
 
 var caixa1 = document.getElementById("input-texto");
 var caixa2 = document.getElementById("msg");
 var btnCripto = document.getElementById("btn-cripto");
+var copybtn = document.getElementById("btn-copy");
 
-btnCripto.addEventListener("click", myFunction);
-
-function myFunction() {
+criptografar = (text) => {
     btnCripto.style.color = "red";
-    caixa2.value = caixa1.value.replace("dio", "Gabriel");
+    chaves.forEach(element => {
+        var textSearch = element[0], textReplace = element[1];
+        text = text.replaceAll(textSearch, textReplace);
+        console.log(text.replaceAll(textSearch, textReplace));
+    });
+    return text;
   }
+
+descriptografar = (text) => {
+  chaves.forEach(element => {
+    var textSearch = element[1], textReplace = element[0];
+    text = replaceAll(textSearch, textReplace);
+  });
+
+  return text;
+}
+
+copy = (textarea) => {
+  textarea.select();
+  textarea.setSelectionRange(0, 99999);
+  navigator.clipboard.writeText(textarea.value);
+  alert("O texto foi copiado");
+  }
+
+copybtn.addEventListener("click", () => {
+  copy(caixa2);
+  copybtn.style.color = "red";
+})
+
+btnCripto.addEventListener("click", () => {
+  let decodeText = caixa1.value, encriptedText = criptografar(decodeText);
+  caixa2.value = encriptedText;
+});
+
